31103a4ef61435f87a8566380b666b362a1e9d86
Daniel Filho <danbsilva98@gmail.com>
2020-08-20 17:21:51 -0300

    v.1

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Projeto 10</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+		<meta name="keywords" content="projeto,estudo,sass">
+		<meta name="author" content="Daniel Filho">
+		<meta name="viewport" content="width=device-width;initial-scale=1.0;maximum-scale=1.0">
+		<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="css/all.min.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<div class="menu-topo">
+					<div class="logo"></div>
+					<nav class="desktop">
+						<ul>
+							<li> <a href="">Sobre</a> </li>
+							<li> <a href="">Serviços</a> </li>
+							<li> <a href="">Contato</a> </li>
+							<li class="btn-nav"> <a href="">Quero um orçamento</a> </li>
+						</ul>
+					</nav> <!-- desktop -->
+					<nav class="mobile">
+						<h3> <i class="fa fa-bars"></i> </h3>
+						<ul>
+							<li> <a href="">Sobre</a> </li>
+							<li> <a href="">Serviços</a> </li>
+							<li> <a href="">Contato</a> </li>
+							<li class="btn-nav"> <a href="">Quero um orçamento</a> </li>
+						</ul>
+					</nav> <!-- mobile -->
+					<div class="clear"></div>
+				</div> <!-- menu-topo -->
+				<div class="chamada-header">
+					<h2>Você vendendo mais<br>com automação web.</h2>
+					<p>Nós criamos soluções de marketing para sua empresa vender mais, da melhor forma.</p>
+					<form>
+						<input type="email" placeholder="Seu e-mail..." required>
+						<input type="submit" name="acao" value="Quero saber mais">
+					</form>
+				</div> <!-- chamada-header -->
+			</div> <!-- container -->
+		</header>
+
+		<section class="descricao-danki">
+			<div class="container">
+				<p>Com anos de experiência em Desenvolvimento Web e Automação de Marketing, a <b>DankiCode</b> traz soluções que ajudam o seu time a vender mais tanto em segmentos B2C quanto B2B.</p>
+			</div> <!-- container -->
+		</section> <!-- descricao-danki -->
+
+		<section class="descricao-danki-2">
+			<div class="container">
+				<div class="texto-descricao">
+					<h2>Nós somos uma agência digital. Trabalhamos seu posicionamento, conteúdo e estética, sempre focado em conversão.</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales finibus auctor. Donec feugiat ligula ac mauris pellentesque, eget mattis orci elementum. Ut feugiat scelerisque quam, in blandit risus fringilla ac.</p>
+				</div> <!-- texto-descricao -->
+				<img alt="Trabalho danki" title="Trabalho danki" src="images/mockup1.jpg">
+				<div class="clear"></div>
+			</div> <!-- container -->
+		</section> <!-- descricao-danki-2 -->
+
+		<section class="descricao-danki-3">
+			<div class="container">
+				<div class="box-depoimento">
+					<p>"Phasellus in hendrerit eros. Ut eget est eget dolor porta euismod quis ut eros. Nullam scelerisque tincidunt leo in elementum. Mauris vitae metus elit."</p><br>
+					<p><b>Jhon Doe - Hoepcke Imóveis</b></p>
+				</div> <!-- box-depoimento -->
+			</div> <!-- container -->
+		</section> <!-- descricao-danki-3 -->
+
+		<section class="servicos">
+			<div class="container">
+				<div class="title-servicos">
+					<h3>Como podemos ajudar sua empresa<br> <i class="fa fa-angle-down"></i> </h3>
+				</div> <!-- title-servicos -->
+				<div class="flex-servicos">
+					<div class="box-servico">
+						<img src="images/img1.jpg">
+						<div class="box-servico-wraper">
+							<h2>E-mail Marketing</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor risus mi, ornare rutrum odio interdum at.</p>
+							<a href="#">Orçamento</a>
+						</div> <!-- box-servico-wraper -->
+					</div> <!-- box-servico -->
+
+					<div class="box-servico">
+						<img src="images/img2.jpg">
+						<div class="box-servico-wraper">
+							<h2>Páginas de Venda</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor risus mi, ornare rutrum odio interdum at.</p>
+							<a href="#">Orçamento</a>
+						</div> <!-- box-servico-wraper -->
+					</div> <!-- box-servico -->
+
+					<div class="box-servico">
+						<img src="images/img3.jpg">
+						<div class="box-servico-wraper">
+							<h2>Páginas de Captura e Inscrição</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor risus mi, ornare rutrum odio interdum at.</p>
+							<a href="#">Orçamento</a>
+						</div> <!-- box-servico-wraper -->
+					</div> <!-- box-servico -->
+
+					<div class="box-servico">
+						<img src="images/img4.jpg">
+						<div class="box-servico-wraper">
+							<h2>SEO & Marketing de conteúdo</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor risus mi, ornare rutrum odio interdum at.</p>
+							<a href="#">Orçamento</a>
+						</div> <!-- box-servico-wraper -->
+					</div> <!-- box-servico -->
+
+					<div class="box-servico">
+						<img src="images/img5.jpg">
+						<div class="box-servico-wraper">
+							<h2>AdWords & Facebook Ads</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor risus mi, ornare rutrum odio interdum at.</p>
+							<a href="#">Orçamento</a>
+						</div> <!-- box-servico-wraper -->
+					</div> <!-- box-servico -->
+
+					<div class="box-servico">
+						<img src="images/img6.jpg">
+						<div class="box-servico-wraper">
+							<h2>Consultoria em Marketing</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor risus mi, ornare rutrum odio interdum at.</p>
+							<a href="#">Orçamento</a>
+						</div> <!-- box-servico-wraper -->
+					</div> <!-- box-servico -->
+				</div> <!-- flex-servicos -->
+			</div> <!-- container -->
+		</section> <!-- servicos -->
+
+		<section class="portfolio">
+			<div class="container">
+				<h2>Algumas empresas que tivemos a oportunidade de atuar:</h2>
+				<div class="empresas">
+					<div class="empresa-single">
+						<img src="images/bianchini.png">
+					</div> <!-- empresa-single -->
+					<div class="empresa-single">
+						<img src="images/rancho.png">
+					</div> <!-- empresa-single -->
+					<div class="empresa-single">
+						<img src="images/hoepcke.png">
+					</div> <!-- empresa-single -->
+					<div class="empresa-single">
+						<img src="images/monguilhott.png">
+					</div> <!-- empresa-single -->
+				</div> <!-- empresas -->
+			</div> <!-- container -->
+		</section> <!-- portfolio -->
+
+		<section class="formulario">
+			<div class="container">
+				<h2>Um orçamento ou alguma dúvida?</h2>
+				<form>
+					<input type="text" name="nome" placeholder="Nome" required>
+					<input type="text" name="email" placeholder="E-mail" required>
+					<input type="text" name="telefone" placeholder="Telefone" required>
+					<textarea name="mensagem" placeholder="Sua mensagem"></textarea>
+					<input type="submit" name="acao"
+					value="Enviar">
+				</form>
+			</div> <!-- container -->
+		</section> <!-- formulario -->
+
+		<footer>
+			<div class="container">
+				<div class="box-footer">
+					<h2>Endereço</h2>
+					<p>Rua Rafael Bandeira, 328 - Centro, Florianópolis/SC, 88015-450, Brasil</p>
+				</div> <!-- box-footer -->
+
+				<div class="box-footer">
+					<h2>Telefone</h2>
+					<p>(48)99901-3620</p>
+				</div> <!-- box-footer -->
+
+				<div class="box-footer">
+					<h2>Receba nossas news</h2>
+					<form>
+						<input placeholder="E-mail" type="text" name="">
+						<button type="submit"> <i class="fa fa-paper-plane"></i> </button>
+					</form>
+				</div> <!-- box-footer -->
+				<div class="clear"></div>
+			</div> <!-- container -->
+		</footer>
+
+		<script type="text/javascript" src="js/jquery-3.5.0.min.js"></script>
+		<script type="text/javascript" src="js/functions.js"></script>
+
+	</body>
+</html>
